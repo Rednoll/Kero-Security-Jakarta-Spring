f1661d0e811c92e786be948fb997c8258df90e28
Kerovazzky <rednoll12@gmail.com>
2020-10-06 11:40:42 +0300

    wip

--- src/main/java/com/kero/security/spring/config/KeroAccessAgentFactoryJakartaNameConfiguration.java
+++ src/main/java/com/kero/security/spring/config/KeroAccessAgentFactoryJakartaNameConfiguration.java
@@ -1,20 +1,27 @@
 package com.kero.security.spring.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
 import com.kero.security.core.agent.KeroAccessAgentFactory;
 import com.kero.security.core.agent.configurator.AccessAgentJakartaNameConfigurator;
 
-@Component
+@Configuration
 public class KeroAccessAgentFactoryJakartaNameConfiguration implements KeroAccessAgentFactorySpringConfiguration {
 	
+	@Value("${kero.security.jakarta.entity.name:true}")
+	private boolean entityName;
+
 	@Autowired
-	private AccessAgentJakartaNameConfigurator config;
+	private AccessAgentJakartaNameConfigurator configurator;
 	
 	@Override
 	public void configure(KeroAccessAgentFactory factory) {
 	
-		factory.addConfigurator(config);
+		if(this.entityName) {
+			
+			factory.addConfigurator(this.configurator);
+		}
 	}
 }
